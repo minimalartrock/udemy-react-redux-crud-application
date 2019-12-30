09a9b74a97581323579621034a08c81ade3108b1
minimalartrock <hiro.team.Vittoria2011@gmail.com>
2019-12-30 11:43:50 +0900

    fix typo for combineReducers

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
-import reducer from "../reducers";
-import App from "./cpmponents/App";
+import reducer from "./reducers";
+import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 
 const store = createStore(reducer);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { CombineReducers } from "redux";
+import { combineReducers } from "redux";
 import count from "./count";
 
 export default combineReducers({ count });
