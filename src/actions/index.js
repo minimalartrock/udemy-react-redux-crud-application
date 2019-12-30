3cedee8bdf57ef63faac9e2970a768085bda3ab8
minimalartrock <hiro.team.Vittoria2011@gmail.com>
2019-12-30 10:13:41 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+export const increment = () => {
+  return { type: INCREMENT };
+};
+
+export const decrement = () => {
+  return { type: DECREMENT };
+};
